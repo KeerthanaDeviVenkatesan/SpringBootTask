118eb21b149488dc61ab537d91c1457a44d755b5
KeerthanaDeviVenkatesan <131868551+KeerthanaDeviVenkatesan@users.noreply.github.com>
2023-10-10 19:10:08 +0530

    Add files via upload

---
+++ WebSocket/src/main/java/com/example/WebSocket/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.WebSocket.controller;
+
+import com.example.WebSocket.entity.User;
+import com.example.WebSocket.entity.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class UserController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/users")
+    public User users(Message message) throws InterruptedException {
+        return new User("Hello!!,    " +
+                HtmlUtils.htmlEscape(message.getName())+".....");
+    }
+}
